7bf82d9c094051688e05c6cbab643bf9cf5d9467
Nicolai.Schoenberg@gmail.com <Nicolai.Schoenberg@gmail.com@cba316e6-74e0-5cab-ceb1-93f47ecaa61e>
2012-09-26 13:48:19 +0000

    + Implemented configuration Window + Implemented BackgroundAction and BackgroundProcess to have a encapsulated thread # Using IoC container instead of creating objects myself - Removed old BootStrapper that is not longer needed

---
+++ TimeKeep/B - Application/ViewModels/ConfigViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TimeKeep.Application.ViewModels
+{
+    using System.ComponentModel.Composition;
+
+    [Export]
+    public class ConfigViewModel
+    {
+    }
+}
--- TimeKeep/B - Application/ViewModels/ShellViewModel.cs
+++ TimeKeep/B - Application/ViewModels/ShellViewModel.cs
@@ -1,102 +1,176 @@
 ﻿namespace TimeKeep.Application.ViewModels {
-  using System;
-  using System.ComponentModel.Composition;
-  using System.Threading;
-  using System.Windows;
-  using System.Windows.Input;
+    using System.ComponentModel.Composition;
+    using System.Windows;
+    using System.Windows.Input;
 
-  using Caliburn.Micro;
+    using Caliburn.Micro;
 
-  using TimeKeep.Domain;
-
-  [Export(typeof(IShell))]
-  public class ShellViewModel : PropertyChangedBase {
-
+    using TimeKeep.Domain;
 
     /// <summary>
-    /// Class that calculates remaining time of round and pause
+    /// The shell view model.
     /// </summary>
-    private RoundManager _roundManager = new RoundManager(new PropertyRoundDefinition());
+    [Export(typeof(IShell))]
+    public class ShellViewModel : PropertyChangedBase {
+   
+        #region Constants and Fields
 
-    private WindowState _windowState;
+        /// <summary>
+        /// The _is fullscreen.
+        /// </summary>
+        private bool _isFullscreen;
 
-    private WindowStyle _windowStyle;
+        /// <summary>
+        ///   Class that calculates remaining time of round and pause
+        /// </summary>
+        private RoundManager _roundManager = new RoundManager(new PropertyRoundDefinition());
 
-    private bool _isFullscreen;
+        /// <summary>
+        /// The _window state.
+        /// </summary>
+        private WindowState _windowState;
 
-    public ShellViewModel() {
-      SetWindowMode();
+        /// <summary>
+        /// The _window style.
+        /// </summary>
+        private WindowStyle _windowStyle;
+
+        private readonly IWindowManager _windowManager;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShellViewModel"/> class.
+        /// </summary>
+        [ImportingConstructor]
+        public ShellViewModel(IWindowManager windowManager) {
+            _windowManager = windowManager;
+            SetWindowMode();
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets RM.
+        /// </summary>
+        public RoundManager RM {
+            get {
+                return _roundManager;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets WinState.
+        /// </summary>
+        public WindowState WinState {
+            get {
+                return _windowState;
+            }
+
+            set {
+                _windowState = value;
+                NotifyOfPropertyChange(() => WinState);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets WinStyle.
+        /// </summary>
+        public WindowStyle WinStyle {
+            get {
+                return _windowStyle;
+            }
+
+            set {
+                _windowStyle = value;
+                NotifyOfPropertyChange(() => WinStyle);
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Opens Config Window
+        /// </summary>
+        public void ShowConfigWindow() {
+            var result = _windowManager.ShowDialog(IoC.Get<ConfigViewModel>());
+        }
+
+        /// <summary>
+        /// Executed ShellView is closed
+        /// </summary>
+        public void OnClosing() {
+            if (_roundManager.IsRunning) {
+                _roundManager.Stop();
+            }
+        }
+
+        /// <summary>
+        /// The on key up.
+        /// </summary>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        public void OnKeyUp(KeyEventArgs e) {
+            if (e.Key == Key.F11) {
+                ToggleFullscreen();
+            }
+        }
+
+        /// <summary>
+        /// The start timer.
+        /// </summary>
+        public void StartTimer() {
+            _roundManager.Start();
+        }
+
+        /// <summary>
+        /// The stop timer.
+        /// </summary>
+        public void StopTimer() {
+            _roundManager.Stop();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// The set fullscreen mode.
+        /// </summary>
+        private void SetFullscreenMode() {
+            WinState = WindowState.Maximized;
+            WinStyle = WindowStyle.None;
+            _isFullscreen = true;
+        }
+
+        /// <summary>
+        /// The set window mode.
+        /// </summary>
+        private void SetWindowMode() {
+            WinState = WindowState.Normal;
+            WinStyle = WindowStyle.SingleBorderWindow;
+            _isFullscreen = false;
+        }
+
+        /// <summary>
+        /// The toggle fullscreen.
+        /// </summary>
+        private void ToggleFullscreen() {
+            if (_isFullscreen) {
+                SetWindowMode();
+            }
+            else {
+                SetFullscreenMode();
+            }
+        }
+
+        #endregion
     }
-
-    public void OnClosing() {
-      if (_roundManager.IsRunning) {
-        _roundManager.StopTimer();
-      }
-    }
-
-    private void ToggleFullscreen() {
-      if (_isFullscreen) {
-        SetWindowMode();
-      }
-      else {
-        SetFullscreenMode();
-      }
-    }
-
-    private void SetWindowMode() {
-      WinState = WindowState.Normal;
-      WinStyle = WindowStyle.SingleBorderWindow;
-      _isFullscreen = false;
-    }
-
-    private void SetFullscreenMode() {
-      WinState = WindowState.Maximized;
-      WinStyle = WindowStyle.None;
-      _isFullscreen = true;
-    }
-
-    public void OnKeyUp(KeyEventArgs e) {
-      if (e.Key == Key.F11) {
-        ToggleFullscreen();
-      }
-    }
-
-    public RoundManager RM {
-      get {
-        return _roundManager;
-      }
-    }
-
-    public WindowStyle WinStyle {
-      
-      get {
-        return _windowStyle;
-      }
-      
-      set {
-        _windowStyle = value;
-        NotifyOfPropertyChange(() => WinStyle);
-      }
-    }
-
-    public WindowState WinState {
-      
-      get {
-        return _windowState;
-      }
-
-      set {
-        _windowState = value;
-        NotifyOfPropertyChange(() => WinState);
-      }
-    }
-
-    public void StartTimer() {
-     _roundManager.StartTimer();
-    }
-
-    public void StopTimer() {
-      _roundManager.StopTimer();
-    }
-  }
 }
